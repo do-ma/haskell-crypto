88cfb9bb5d928a4d63ecac122675bc938646067d
Jonn <jonn@doma.dev>
2021-01-05 12:23:02 +0000

    Problem: alloc / allocRet semantics are confusing
    Solution:
    
     - Tidy up unnecessary `pure` / `return` to reduce confusion
     - Between using `allocRet` everywhere (which I'd personally prefer) and
    	using `alloc` for public key, I would choose using `allocRet`
    	everywhere, because it makes code "self-documenting":
    
    ```
    keypair :: IO (PublicKey ByteString, SecretKey ScrubbedBytes)
    keypair =
      Sized.allocRet Proxy $ \skPtr -> do
        ((), pk) <- Sized.allocRet Proxy $ \pkPtr ->
          void $ Na.crypto_sign_keypair pkPtr skPtr
        return pk
    ```
    
    	however, the author of the library insists on using `alloc`, so
    this commit introduces excessive documentation to a function that got me
    personally very confused.

--- NaCl/lib/NaCl/Sign/Internal.hs
+++ NaCl/lib/NaCl/Sign/Internal.hs
@@ -56,14 +56,27 @@ toPublicKey = sizedByteArray
 -- @Crypto.Init@ in
 -- <https://hackage.haskell.org/package/crypto-sodium crypto-sodium>
 -- to learn how to make this function thread-safe.
+--
+-- What this function does is really:
+--  1. In Sized.allocRet invocation:
+--    A. Allocate 64 bytes by `Proxy` that is inferred to be `:: Proxy 64` because
+--       `64 ~ Na.CRYPTO_SIGN_SECRETKEYBYTES` and `SecretKey a = SizedByteArray Na.CRYPTO_SIGN_SECRETKEYBYTES a`.
+--    B. Run underlying function (2) and return a tuple the first element of which
+--       is its output, and the second element of which is bytes that end up under the
+--       pointer bound by (1).
+--  2. In Sized.alloc invocation, infer the size of public key and:
+--    A. Run Na.crypto_sign_keypair that populates data under pkPtr and skPtr
+--    B. Return just the data under pkPtr from `alloc`
+-- This way, data from pkPtr gets into the first element of (1)'s return value!
 keypair :: IO (PublicKey ByteString, SecretKey ScrubbedBytes)
-keypair = do
-  (pk, sk) <-
-    Sized.allocRet Proxy $ \skPtr ->
+keypair =
+  -- allocRet returns a tuple consisiting of
+  --  1. Whatever underlying `IO a` function returns
+  --  2. Contents of allocated Ptr
+  Sized.allocRet Proxy $ \skPtr -> do
+    -- alloc returns contents of pkPtr after executing side effect
     Sized.alloc $ \pkPtr ->
-    -- always returns 0, so we don’t check it
-    void $ Na.crypto_sign_keypair pkPtr skPtr
-  pure (pk, sk)
+      void $ Na.crypto_sign_keypair pkPtr skPt
 
 
 -- | Sign a message.
